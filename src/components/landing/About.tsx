19625a2b686726777cc4bc8753a8bf4afd788e11
podestal <luisrodriguezpodesta@gmail.com>
2024-07-09 13:30:43 -0500

    about done

--- src/components/landing/About.tsx
+++ src/components/landing/About.tsx
@@ -1,6 +1,7 @@
 import useLanguageStore from "../../store/store"
 import { aboutData } from "../../data/landing"
 import profilePic from '../../assets/imgs/developer-pic.png'
+import { motion } from "framer-motion"
 
 const About = () => {
 
@@ -9,14 +10,22 @@ const About = () => {
 
   return (
     <section className="max-lg:flex-col min-h-screen flex justify-center items-center gap-10">
-        <div className="w-full flex flex-col justify-start items-center gap-6">
+        <motion.div 
+            initial={{opacity: 0, translateX: -100}}
+            whileInView={{opacity: 1, translateX: 0}}
+            transition={{duration: 1.2}}
+            className="w-full flex flex-col justify-start items-center gap-6">
             <h2 className="text-5xl lg:text-8xl text-center py-6">{data.title}</h2>
             <img src={profilePic} alt="" />
-        </div>
-        <div className="w-full flex flex-col justify-center items-center min-h-screen bg-slate-900 gap-6 font-montserrat">
+        </motion.div>
+        <motion.div 
+            initial={{opacity: 0, translateX: 100}}
+            whileInView={{opacity: 1, translateX: 0}}
+            transition={{duration: 1.2}}
+            className="w-full flex flex-col justify-center items-center min-h-screen bg-slate-900 gap-6 font-montserrat">
             <h3 className="lg:text-7xl text-4xl">{data.aboutTitle}</h3>
             <p className="text-slate-50 lg:leading-8 lg:text-xl font-poppins text-left w-[60%]">{data.description}</p>
-        </div>
+        </motion.div>
     </section>
   )
 }
--- src/components/landing/Contact.tsx
+++ src/components/landing/Contact.tsx
@@ -15,32 +15,36 @@ const Contact = () => {
             whileInView={{opacity: 1, translateY: 0}}
             transition={{duration: 1.2}}
             className="flex flex-col justify-center items-center gap-6">
-            <h2 className="lg:text-7xk text-6xl">{data.title}</h2>
+            <h2 className="lg:text-7xk text-6xl text-center">{data.title}</h2>
             <p>{data.subTitle}</p>
         </motion.div>
         <form className="w-full flex flex-col gap-12 justify-center items-center lg:text-xl">
-            <div className="flex justify-center items-center gap-8">
+            <div className="max-lg:w-[75%] flex justify-center max-lg:flex-col items-center gap-8">
                 <motion.div 
                     initial={{opacity: 0, translateX: -100}}
                     whileInView={{opacity: 1, translateX: 0}}
                     transition={{duration: 1.2}}
-                    className="flex flex-col justify-center items-center gap-4">
+                    className="w-full flex flex-col justify-center items-center gap-4">
                     <p>{data.nameLabel}</p>
-                    <TextInput placeholder={data.namePlaceHolder} className="lg:w-[450px]"/>
+                    <TextInput placeholder={data.namePlaceHolder} className="w-full lg:w-[450px]"/>
                 </motion.div>
                 <motion.div
                     initial={{opacity: 0, translateX: 100}}
                     whileInView={{opacity: 1, translateX: 0}}
                     transition={{duration: 1.2}}
-                    className="flex flex-col justify-center items-center gap-4">
+                    className="max-lg:w-full flex flex-col justify-center items-center gap-4">
                     <p>{data.emailLabel}</p>
                     <TextInput placeholder={data.emailPlaceHolder} className="lg:w-[450px]"/>
                 </motion.div>
             </div>
-            <div className="w-full flex flex-col justify-center items-center gap-4">
+            <motion.div 
+                initial={{opacity: 0, translateY: 100}}
+                whileInView={{opacity: 1, translateY: 0}}
+                transition={{duration: 1.2}}
+                className="max-lg:w-[75%] w-full flex flex-col justify-center items-center gap-4">
                 <p>{data.messageLabel}</p>
                 <Textarea placeholder={data.messagePlaceHolder} className="px-6 py-6 lg:w-[750px] h-[200px]"/>
-            </div>
+            </motion.div>
             <Button color="blue">{data.buttonText}</Button>
         </form>
     </section>
